c0166c725846f531555ac1e93e90fea3b69901cd
Quarps <landerzen10@gmail.com>
2025-02-21 15:05:07 +0100

    preparing for db

--- project/server.js
+++ project/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// const db = require("./db");
+
 //--- Middleware ----------------------------------------------------
 app.use(express.json());
 app.use("/", express.static("public"));
@@ -10,6 +12,17 @@ app.use("/", express.static("public"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+/*
+  db.all("SELECT * FROM storeName", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+*/
 
 //--- Server --------------------------------------------------------
 app.listen(PORT, () => {
